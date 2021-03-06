3184e82cc5aa2a0f657ff9fe82e7e8d6e2f51eb8
dkeddie <mail@davidkeddie.com>
2021-03-06 17:35:23 +0000

    Added adjust bag and remove from bag views, and calc subtotal template filter

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -35,3 +35,51 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag #overwrites session variable with updated version
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    bag = request.session.get('bag', {}) #get's session 'bag' or creates one if it doesn't exist yet
+
+    if size:
+        if quantity > 0:
+            bag[item_id]['items_by_size'][size] = quantity
+        else:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['item_by_size']: #to remove item, so item with zero qty NOT left in bag
+                bag.pop(item_id) 
+    else:
+        if quantity > 0:
+            bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag.pop(item_id)
+
+    request.session['bag'] = bag #overwrites session variable with updated version
+    return redirect(reverse('view_bag'))
+
+def remove_from_bag(request, item_id):
+    """ Remove the item from the shopping bag"""
+
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+        if size:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
