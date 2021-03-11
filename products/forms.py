6994711e520ac75e9e8a9eb34d9f5593120ba6b2
dkeddie <mail@davidkeddie.com>
2021-03-11 21:27:32 +0000

    Product admin - Finished add product functionality

--- products/forms.py
+++ products/forms.py
@@ -9,17 +9,15 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
 
-        # to override the __init__ to make some changes to the fields
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            categories = Category.objects.all()
-            # create a list of tuples of the friendly names associated with their category ids
-            # syntax = list comprehension, short hand way of adding items to a list
-            friendly_name = [(c.id, c.get_friendly_name()) for c in categories]
+    # to override the __init__ to make some changes to the fields
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        # create a list of tuples of the friendly names associated with their category ids
+        # syntax = list comprehension, short hand way of adding items to a list
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
-            # update caegory field to use friendly name for choices instead of ID
-            self.fields['category'].choices = friendly_names
-            for field_name, field in self.fields.items():
-                field.widget.attrs['class'] = 'border-black rounded-0'
-
-
+        # update caegory field to use friendly name for choices instead of ID
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
