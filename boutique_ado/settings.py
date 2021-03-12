b182437c5e2b77988131a2b64ce0bf1a05267011
dkeddie <mail@davidkeddie.com>
2021-03-12 22:45:09 +0000

    fix settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -122,7 +122,7 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if 'DATABASE_URL' os.environ:
+if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
